a69740829cbe0cc22474212647efb1cfa2808730
vlad <vlad13pon13@gmail.com>
2025-08-10 16:52:46 +0300

    Задание 5. Добавление двух тестов на создание группы контактов

---
+++ addressbook-web-tests/src/test/java/CreateGroupTest.java
@@ -0,0 +1,67 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class CreateGroupTest {
+    private static WebDriver driver;
+
+    @BeforeEach
+    public void setUp() {
+        if (driver == null) {
+            driver = new FirefoxDriver();
+            Runtime.getRuntime().addShutdownHook(new Thread(driver::quit)); //дополнительно изучить для понимания
+            driver.get("http://localhost/addressbook/");
+            driver.manage().window().setSize(new Dimension(1920, 1080));
+            driver.findElement(By.name("user")).sendKeys("admin");
+            driver.findElement(By.name("pass")).sendKeys("secret");
+            driver.findElement(By.xpath("//input[@value=\'Login\']")).click();
+        }
+
+    }
+
+    @Test
+    @DisplayName("Создание группы с текстовыми заголовоками")
+    public void canCreateGroup() {
+        if (!isElementPresent(By.name("new"))) {
+            driver.findElement(By.linkText("groups")).click();
+        }
+        driver.findElement(By.name("new")).click();
+        driver.findElement(By.name("group_name")).click();
+        driver.findElement(By.name("group_name")).sendKeys("FirstGroup");
+        driver.findElement(By.name("group_header")).sendKeys("header");
+        driver.findElement(By.name("group_footer")).sendKeys("comment");
+        driver.findElement(By.name("submit")).click();
+        driver.findElement(By.linkText("group page")).click();
+
+    }
+
+    @Test
+    @DisplayName("Создание группы с пустыми заголовками")
+    public void canCreateEmptyTitleGroup() {
+        if (!isElementPresent(By.name("new"))) {
+            driver.findElement(By.linkText("groups")).click();
+        }
+        driver.findElement(By.name("new")).click();
+        driver.findElement(By.name("group_name")).click();
+        driver.findElement(By.name("group_name")).sendKeys("");
+        driver.findElement(By.name("group_header")).sendKeys("");
+        driver.findElement(By.name("group_footer")).sendKeys("");
+        driver.findElement(By.name("submit")).click();
+        driver.findElement(By.linkText("group page")).click();
+    }
+
+    private boolean isElementPresent(By locator) {
+        try {
+            driver.findElement(locator);
+            return true;
+        } catch (NoSuchElementException exception) {
+            return false;
+        }
+
+    }
+}
