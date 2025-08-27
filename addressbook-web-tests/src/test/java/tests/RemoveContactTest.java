5ce40377fd463d099abda625eafc5bd9b42d0c30
vlad <vlad13pon13@gmail.com>
2025-08-27 20:02:33 +0300

    refactor RemoveContactTest

--- addressbook-web-tests/src/test/java/tests/RemoveContactTest.java
+++ addressbook-web-tests/src/test/java/tests/RemoveContactTest.java
@@ -1,6 +1,7 @@
 package tests;
 
 import models.ContactData;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class RemoveContactTest extends TestBase{
@@ -8,8 +9,8 @@ public class RemoveContactTest extends TestBase{
 
     @Test
     public void removeContactsTest(){
-        if(!app.contact().isContactPresent()){
-            ContactData data = new ContactData(
+        if(app.contact().countContact() == 0){
+            app.contact().createNewContract(new ContactData(
                     "John",
                     "A.",
                     "Doe",
@@ -19,10 +20,34 @@ public class RemoveContactTest extends TestBase{
                     "123 Elm Street",
                     "555-5678",
                     "john.doe@example.com"
-            );
-            app.contact().createNewContract(data);
+            ));
+            app.contact().createNewContract(new ContactData(
+                    "Jane",
+                    "A.",
+                    "Doe",
+                    "JaneSun",
+                    "she",
+                    "Acme",
+                    "123 Elm Street",
+                    "555-5678",
+                    "Jane.Doe@example.com"));
+        } else if(app.contact().countContact() == 1){
+            app.contact().createNewContract(new ContactData(
+                    "Jane",
+                    "A.",
+                    "Doe",
+                    "JaneSun",
+                    "she",
+                    "Acme",
+                    "123 Elm Street",
+                    "555-5678",
+                    "Jane.Doe@example.com"));
+
         }
+        int contactBeforeTest = app.contact().countContact();
         app.contact().removeAllContact();
+        int contactAfterTest = app.contact().countContact();
+        Assertions.assertEquals(0, contactAfterTest);
     }
 
 
