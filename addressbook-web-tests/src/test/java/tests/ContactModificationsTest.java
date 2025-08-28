209844f70f28e20bb29011c1059b5bd3af819cc9
vlad <vlad13pon13@gmail.com>
2025-08-29 02:55:15 +0300

    Задание №11: Сделать тесты для модификации групп и модификации контактов

--- addressbook-web-tests/src/test/java/tests/ContactModificationsTest.java
+++ addressbook-web-tests/src/test/java/tests/ContactModificationsTest.java
@@ -1,40 +1,55 @@
 package tests;
 
 import models.ContactData;
-import models.GroupData;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class ContactModificationsTest extends TestBase{
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Random;
+
+public class ContactModificationsTest extends TestBase {
 
     @Test
     @DisplayName("Изменение существующего контакта")
-    public void modifyContactTest(){
-        if (app.contact().countContact() == 0){
+    public void modifyContactTest() {
+        if (app.contact().countContact() == 0) {
             app.contact().createNewContract(new ContactData(
                     "John",
-                    "A.",
                     "Doe",
-                    "Johnny",
-                    "he",
-                    "Acme",
                     "123 Elm Street",
                     randomPhoneNumber(),
                     "john.doe@example.com"
             ));
-
         }
-        app.contact().modifyContact(new ContactData("John",
-                "A.",
+
+        var oldContacts = app.contact().getList();
+        Random rnd = new Random();
+        int index = rnd.nextInt(oldContacts.size());
+
+        ContactData modifyData = new ContactData(
+                generateRandomName("John"),
                 "Doe",
-                "Johnny_123",
-                "he",
-                "Acme",
                 "123 Elm Street",
                 randomPhoneNumber(),
-                "john.doe@example.com"));
+                "john.doe@example.com");
+
+        modifyData.setId(oldContacts.get(index).getId());
+        modifyData.setMobile(oldContacts.get(index).getMobile());
+        app.contact().modifyContact(oldContacts.get(index),modifyData);
+
+        var newContacts = app.contact().getList();
+        var expectedList = new ArrayList<>(oldContacts);
+        expectedList.set(index, modifyData);
+        Comparator<ContactData> compareById = (o1, o2) -> {
+            return Integer.compare(Integer.parseInt(o1.getId()), Integer.parseInt(o2.getId()));
+        };
+        newContacts.sort(compareById);
+        expectedList.sort(compareById);
+
+        Assertions.assertEquals(newContacts, expectedList);
 
     }
-
 
 }
--- addressbook-web-tests/src/test/java/tests/GroupModificationsTests.java
+++ addressbook-web-tests/src/test/java/tests/GroupModificationsTests.java
@@ -1,7 +1,13 @@
 package tests;
 
 import models.GroupData;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Random;
 
 public class GroupModificationsTests extends TestBase{
 
@@ -10,8 +16,23 @@ public class GroupModificationsTests extends TestBase{
         if (app.group().getGroupCount()==0){
             app.group().createGroup(new GroupData().withName("forModifyTest"));
         }
-        app.group().modifyGroup(new GroupData().withName("ModifyTestName"));
 
+        List<GroupData> oldGroups = app.group().getList();
+        Random rnd = new Random();
+        int index = rnd.nextInt(oldGroups.size());
+
+        GroupData testDate = new GroupData().withName("ModifyTestName");
+        app.group().modifyGroup(oldGroups.get(index),testDate);
+
+        List<GroupData> newGroups = app.group().getList();
+        List<GroupData> expectedList = new ArrayList<>(oldGroups);
+        expectedList.set(index, testDate.withHId(oldGroups.get(index).id()));
+        Comparator<GroupData> compareById = (o1, o2) -> {
+            return Integer.compare(Integer.parseInt(o1.id()), Integer.parseInt(o2.id()));
+        };
+        newGroups.sort(compareById);
+        expectedList.sort(compareById);
+        Assertions.assertEquals(newGroups, expectedList);
 
     }
 
