d6551d43eae994e1403396f72cab211d61ee1fae
vlad <vlad13pon13@gmail.com>
2025-09-03 15:09:42 +0300

    Добавление провайдера, который читает из файла тест дату

--- addressbook-web-tests/src/test/java/tests/CreateGroupTest.java
+++ addressbook-web-tests/src/test/java/tests/CreateGroupTest.java
@@ -1,5 +1,6 @@
 package tests;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import coomon.CommonFunctions;
 import models.GroupData;
 import org.junit.jupiter.api.Assertions;
@@ -8,19 +9,21 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Stream;
 
 
-public class CreateGroupTest extends TestBase{
+public class CreateGroupTest extends TestBase {
 
     @ParameterizedTest
     @DisplayName("Создание групп по 4 сценариям")
     @MethodSource("providerGroupData")
     public void canCreateGroup(GroupData Data) {
-        int groupCountBefore= app.group().getGroupCount();
+        int groupCountBefore = app.group().getGroupCount();
         app.group().createGroup(Data);
         int groupCountAfter = app.group().getGroupCount();
 
@@ -33,7 +36,7 @@ public class CreateGroupTest extends TestBase{
     @ParameterizedTest
     @DisplayName("Создание провайдера с циклами и вложенными циклами")
     @MethodSource("positiveGroupNameProvider")
-    public void canCreateGroupCycle(GroupData groupData){
+    public void canCreateGroupCycle(GroupData groupData) {
 
         List<GroupData> oldGroups = app.group().getList();
 
@@ -46,7 +49,7 @@ public class CreateGroupTest extends TestBase{
         newGroups.sort(compareById);
 
         List<GroupData> expectedList = new ArrayList<>(oldGroups);
-        expectedList.add(groupData.withHId(newGroups.get(newGroups.size()-1).id()).withHeader("").withFooter(""));
+        expectedList.add(groupData.withHId(newGroups.get(newGroups.size() - 1).id()).withHeader("").withFooter(""));
         expectedList.sort(compareById);
         Assertions.assertEquals(newGroups, expectedList);
 
@@ -55,7 +58,7 @@ public class CreateGroupTest extends TestBase{
 
     @ParameterizedTest
     @MethodSource("negativeGroupCreation")
-    public void negativeCreateGroup(GroupData data){
+    public void negativeCreateGroup(GroupData data) {
         var oldGroups = app.group().getList();
         app.group().createGroup(data);
         var newGroups = app.group().getList();
@@ -63,7 +66,34 @@ public class CreateGroupTest extends TestBase{
 
     }
 
-    static Stream<Arguments> providerGroupData(){
+    @ParameterizedTest
+    @DisplayName("Создание c провайдером, который берет данные из файла ")
+    @MethodSource("readFileProvider")
+    public void canCreateGroupJsonProvider(GroupData groupData) {
+
+        List<GroupData> oldGroups = app.group().getList();
+
+        app.group().createGroup(groupData);
+
+        List<GroupData> newGroups = app.group().getList();
+        Comparator<GroupData> compareById = (o1, o2) -> {
+            return Integer.compare(Integer.parseInt(o1.id()), Integer.parseInt(o2.id()));
+        };
+        newGroups.sort(compareById);
+
+        List<GroupData> expectedList = new ArrayList<>(oldGroups);
+        expectedList.add(groupData.withHId(newGroups.get(newGroups.size() - 1).id()).withHeader("").withFooter(""));
+        expectedList.sort(compareById);
+        Assertions.assertEquals(newGroups, expectedList);
+
+
+    }
+
+
+
+
+
+    static Stream<Arguments> providerGroupData() {
         return Stream.of(
                 Arguments.of(new GroupData().withHeader("testHeader")),
                 Arguments.of(new GroupData().withName("testName")),
@@ -72,17 +102,17 @@ public class CreateGroupTest extends TestBase{
         );
     }
 
-    static List<GroupData> positiveGroupNameProvider(){
+    static List<GroupData> positiveGroupNameProvider() {
         ArrayList<GroupData> list = new ArrayList<>();
-        for (String name : List.of("", "group_name")){
-            for (String header : List.of( "", "header_name")){
-                for (String footer : List.of("", "footer_name")){
-                    list.add(new GroupData("", name,footer,header));
+        for (String name : List.of("", "group_name")) {
+            for (String header : List.of("", "header_name")) {
+                for (String footer : List.of("", "footer_name")) {
+                    list.add(new GroupData("", name, footer, header));
                 }
             }
         }
 
-        for (int i = 0; i<5; i++){
+        for (int i = 0; i < 5; i++) {
             list.add(new GroupData().
                     withName(CommonFunctions.randomString(i * 10)).
                     withHeader(CommonFunctions.randomString(i * 10)).
@@ -92,15 +122,21 @@ public class CreateGroupTest extends TestBase{
         return list;
     }
 
-    static List<GroupData> negativeGroupCreation(){
+    static List<GroupData> negativeGroupCreation() {
         ArrayList<GroupData> listData = new ArrayList<>(List.of(
                 new GroupData().withName("group_name'").withHeader(CommonFunctions.randomString(5)).withFooter(CommonFunctions.randomString(5))
         ));
         return listData;
     }
 
+    static List<GroupData> readFileProvider() throws IOException {
+        var result = new ArrayList<GroupData>();
+        var resultFile = mapper.readValue(new File("groupsData.json"), new TypeReference<List<GroupData>>() {});
+        result.addAll(resultFile);
+        return result;
 
 
+    }
 
 
 }
--- addressbook-web-tests/src/test/java/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/tests/TestBase.java
@@ -1,11 +1,14 @@
 package tests;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import manager.ApplicationManager;
 import org.junit.jupiter.api.BeforeEach;
 
 public class TestBase {
 
     protected static ApplicationManager app;
+    public static ObjectMapper mapper = new ObjectMapper();
+
 
     @BeforeEach
     public void setUp(){
